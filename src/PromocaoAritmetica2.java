442698e9c1d095b4da497a5c4c30dc8d6edd1fea
Henrique Nascimento <114836223+Henryborg@users.noreply.github.com>
2023-03-21 23:10:15 -0300

    add
    add

---
+++ src/PromocaoAritmetica2.java
@@ -0,0 +1,12 @@
+public class PromocaoAritmetica2 {
+  
+  public static void main(String[] args)  {
+    long x = 10;
+    float y = 5f;
+    float z = x * y; 
+
+    System.out.println(z);
+
+  }
+
+}
---
+++ src/PromocaoAritmetica3.java
@@ -0,0 +1,12 @@
+public class PromocaoAritmetica3 {
+  
+  public static void main(String[] args)  {
+    int x = 3;
+    int y = 2;
+    float z = x / y; 
+
+    System.out.println(z);
+
+  }
+
+}
